9538714d872b484a1d75e682bf0e9983e562b729
Te Engajei <teengajei@gmail.com>
2026-03-09 21:05:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5303680064-d9d6f",
+  "appId": "1:626490307446:web:c769561679d4a48ea28057",
+  "apiKey": "AIzaSyAj2AAOKmxlYAlsbSxyj8r2ubLSlOVuzuQ",
+  "authDomain": "studio-5303680064-d9d6f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626490307446"
+};
